a82c87d0ccb5c6bccebde7ce0cf67d1b5f5df7cf
DavidTu <a3102a123@gmail.com>
2024-09-02 16:04:27 +0800

    add requirements file & using env var to save the token

--- main.py
+++ main.py
@@ -3,9 +3,14 @@ import json
 import os
 import asyncio
 
-with open(os.path.join("config.json")) as json_file:
-    config = json.load(json_file)
-print(config)
+try:
+    TOKEN = os.getenv("DC_TOKEN")
+    print("Finding the env var of token")
+except:
+    with open(os.path.join("config.json")) as json_file:
+        config = json.load(json_file)
+        TOKEN = config["TOKEN"]
+    print("Using config file to set up token")
 
 # init the bot
 ### intents is the authority of the discord 
@@ -13,4 +18,4 @@ intents = discord.Intents.default()
 intents.message_content = True
 client = BotClient(intents=intents)
 ### run the bot
-client.run(config["TOKEN"])
+client.run(TOKEN)
